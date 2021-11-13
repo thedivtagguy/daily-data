1665d5f4934582371eadc0e5972b45247cbe327a
Aman Bhargava <amanbhargava2001@gmail.com>
2021-11-14 01:26:30 +0530

    Added Gender Recognition

--- dd02_commonSenseMedia/common_sense_media_analysis.r
+++ dd02_commonSenseMedia/common_sense_media_analysis.r
@@ -6,6 +6,10 @@ library(ggtext)    # Using HTML in ggplot text
 library(gridExtra) # functions to work with pictures
 library(grid)
 library(corpus)
+library(extrafont)
+
+font_import("./resources/fonts/grand_royal/")
+loadfonts(device = "win", quiet = TRUE) 
 
 dd <- "dd02_common_sense_media"
 
@@ -22,7 +26,7 @@ data <- data %>%
 # Exploratory Analysis
 
 # Mean difference in the ages that parents recommend and those that kids recommend, based on genre
-data %>%
+mean <- data %>%
   group_by(genres) %>%
   summarise(difference = (mean(parents_say, na.rm = TRUE) - mean(kids_say, na.rm = TRUE))) %>%
   drop_na() %>%
@@ -30,48 +34,152 @@ data %>%
   mutate(genres = reorder(genres, difference)) %>%
   ggplot(., aes(x = difference, y = genres, fill = difference < 0)) +
   geom_col(show.legend = FALSE) +
+  scale_fill_manual(values = c("#E63946", "#457B9D")) +
   theme_minimal() +
   labs(
     x = "Mean Difference Between Parent Recommended Age <br> and Kids Recommended Age",
     y = "Genres",
-    title = "Parents say 16, Kids say 12...",
+    title = "Parents say <span style='font-size:30px;'>16</span>, kids say <span style='font-size:30px;'>12</span>",
     subtitle = "Examining the mean difference in viewing ages <br> recommended by parents versus kids based on genre"
   ) +
   annotate(
-    geom = "curve", x = -0.02, y = 21.5, xend = -0.4, yend = 18, 
-    curvature = .3, arrow = arrow(length = unit(2, "mm"))
+    geom = "curve",
+    x = -0.02,
+    colour = "#8A6552",
+    y = 21.5,
+    xend = -0.4,
+    yend = 18,
+    curvature = .3,
+    arrow = arrow(length = unit(2, "mm"))
   ) +
-  annotate(geom = "text", x = -0.7, y = 17, size = 3.5, label = "Parents say kids need to be older", hjust = "left") +
   annotate(
-    geom = "curve", x = 0.02, y = 1, xend = 0.4, yend = 3, 
-    curvature = .3, arrow = arrow(length = unit(2, "mm"))
+    geom = "text",
+    x = -0.8,
+    y = 17,
+    size = 3.5,
+    label = "Parents say kids need to be older",
+    hjust = "left"
   ) +
-  annotate(geom = "text", x = 0.15, y = 3.8, size = 3.5, label = "Kids say they need to be older", hjust = "left") +
+  annotate(
+    geom = "curve",
+    x = 0.02,
+    y = 1,
+    colour = "#8A6552",
+    xend = 0.4,
+    yend = 3,
+    curvature = .3,
+    arrow = arrow(length = unit(2, "mm"))
+  ) +
+  annotate(
+    geom = "text",
+    x = 0.15,
+    y = 3.8,
+    size = 3.5,
+    label = "Kids say they need to be older",
+    hjust = "left"
+  ) +
   
   theme(
-    plot.title = element_markdown(margin = margin(t = 0.2, b = 0.2, unit = "cm")),
-    plot.subtitle = element_markdown(margin = margin(b = 0.5, unit = "cm")),
-    plot.background = element_rect(fill = "#FEE1C7"),
-    axis.title.x = element_markdown(margin = margin(t = 0.5, b = 0.5, unit = "cm" ))
-    ) 
+    plot.margin = unit(c(1, 2, 1, 2), "cm"),
+    plot.title = element_markdown(
+      margin = margin(t = 0.2, b = 0.2, unit = "cm"),
+      size = 26,
+      family = "Grand Royal",
+      face = "plain",
+      colour = "#4C1E4F"
+    ),
+    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), family="Raleway", face = "plain",colour = "#3F2F27"),
+    plot.background = element_rect(fill = "#FEE1C7", color = NA),
+    axis.line = element_line(colour = "#3F2F27"),
+    panel.grid = element_blank(),
+    axis.title.x = element_markdown(
+      margin = margin(t = 0.5, b = 0.5, unit = "cm"),
+      face = "bold",
+      colour = "#4C1E4F"
+    ),
+    axis.title.y = element_markdown(
+      margin = margin(t = 0.5, b = 0.5, unit = "cm"),
+      face = "bold",
+      colour = "#4C1E4F"
+    ),
+    axis.text.y = element_markdown(colour = "#3F2F27"),
+    axis.text.x = element_markdown(colour = "#3F2F27")
+  )
 
 # What words are associated with what ratings for language?
-data %>%
+language <- data %>%
   group_by(language_rating) %>%
   drop_na() %>%
-  summarise(words = 
-    str_extract_all(
-    str_replace_all(
-      str_to_lower(language_text), '[.,]', ""), '"(.*?)"'
-  )) %>%
-  unnest(cols = c(words)) %>% 
-  mutate(words = str_replace_all(words, '"',"")) %>%
-  na_if("") %>% 
-  mutate(words = trimws(words, which = "both")) %>% 
+  summarise(words =
+              str_extract_all(str_replace_all(str_to_lower(language_text), '[.,]', ""), '"(.*?)"')) %>%
+  unnest(cols = c(words)) %>%
+  mutate(words = str_replace_all(words, '"', "")) %>%
+  mutate(words = str_replace_all(words, "a--hole", "ass")) %>% 
+  mutate(words = str_replace_all(words, "jesus christ", "christ")) %>% 
+  na_if("") %>%
+  mutate(words = trimws(words, which = "both")) %>%
   drop_na() %>%
   count(language_rating, words, sort = TRUE) %>%
   slice_max(order_by = n, n = 4) %>%
-  arrange(desc(language_rating, n)) %>% 
+  arrange(desc(language_rating, n)) %>%
   filter(words != "" & language_rating != "0") %>% 
-  ggplot(aes())
-  
+  mutate(words = fct_reorder(words, n))
+
+language_plot <- language %>% 
+  ggplot(aes(x = language_rating, y = n, fill = language_rating))+
+  geom_point(size = 0, fill = NA, colour = NA)+ 
+  geom_text(
+    label=paste(toupper(language$words), language$n, sep = " - "),
+    nudge_x = 0.1,  
+    check_overlap = T,
+    size = 3,
+    family = "Raleway",
+    fontface = "bold",
+    colour = "#3F2F27"
+    )+
+  labs(title = "How bad are words?",
+       subtitle = "CSM rates movies on the kind of language they contain. <br> What are the most common words in each ranking?",
+       x = "Language rating",
+       y = "Number of times mentioned")+
+  theme_minimal()+
+  theme(
+    plot.margin = unit(c(1, 2, 1, 2), "cm"),
+    plot.title = element_markdown(
+      margin = margin(t = 0.2, b = 0.2, unit = "cm"),
+      size = 26,
+      family = "Grand Royal",
+      face = "plain",
+      colour = "#4C1E4F"
+    ),
+    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), lineheight = 1.2, family="Raleway", face = "plain",colour = "#3F2F27"),
+    plot.background = element_rect(fill = "#FEE1C7", color = NA),
+    axis.line = element_line(colour = "#3F2F27"),
+    panel.grid = element_blank(),
+    axis.title.x = element_markdown(
+      margin = margin(t = 0.5, b = 0.5, unit = "cm"),
+      face = "bold",
+      colour = "#4C1E4F"
+    ),
+    axis.title.y = element_markdown(
+      margin = margin(r = 0.5, l = 0.5, unit = "cm"),
+      face = "bold",
+      colour = "#4C1E4F"
+    ),
+    axis.text.y = element_markdown(colour = "#3F2F27", hjust = 3, size = 10, family = "Raleway", face ="bold"),
+    axis.text.x = element_markdown(colour = "#3F2F27", size = 15, family = "Raleway", face ="bold", margin = margin(t = 10))
+  )
+
+mean + language_plot
+
+### Ratings by genre
+data %>% 
+  group_by(genres) %>% 
+  pivot_longer(c(diversity_rating, language_rating, positive_rating, role_models_rating, violence_rating, sex_rating, language_rating, consumerism_rating, drugs_rating), values_to = "rating", names_to = "category") %>% 
+  select(genres, category, rating) %>% 
+  ggplot(aes(x = genres, y = rating, fill = category)) + 
+  geom_bar(position = "dodge", stat= "identity")
+
+# Save
+plot_name <- "common_sense"
+ggsave(glue::glue("./{plot_name}.png"), width = 40, height = 20, units = "cm")
+
