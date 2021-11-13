4fad845391c7eecadc87b2516e8978b1567775cc
Aman Bhargava <amanbhargava2001@gmail.com>
2021-11-13 13:10:13 +0530

    dd01 - Reverse direction

--- dd02_commonSenseMedia/common_sense_media_analysis.r
+++ dd02_commonSenseMedia/common_sense_media_analysis.r
@@ -26,5 +26,33 @@ data %>%
   group_by(genres) %>%
   summarise(difference = (mean(parents_say, na.rm = TRUE) - mean(kids_say, na.rm = TRUE))) %>%
   drop_na() %>%
-  ggplot(., aes(x = genres, y = difference)) +
-  geom_bar(stat = "identity")
+  arrange(desc(difference)) %>%
+  mutate(genres = reorder(genres, difference)) %>%
+  ggplot(., aes(x = difference, y = genres, fill = difference < 0)) +
+  geom_col(show.legend = FALSE) +
+  theme_minimal() +
+  labs(
+    x = "Mean Difference Between Parent Recommended Age <br> and Kids Recommended Age",
+    y = "Genres",
+    title = "Parents say 16, Kids say 12...",
+    subtitle = "Examining the mean difference in viewing ages <br> recommended by parents versus kids based on genre"
+  ) +
+  theme(
+    axis.title.x = element_markdown(margin = margin(
+      t = 0.5, b = 0.5, unit = "cm"
+    )),
+    plot.title = element_markdown(margin = margin(
+      t = 0.2, b = 0.2, unit = "cm"
+    )),
+    plot.subtitle = element_markdown(margin = margin(b = 0.5, unit = "cm"))
+  )
+
+# What words are associated with what ratings for language?
+language <- data %>%
+  group_by(language_rating) %>%
+  drop_na() %>%
+  summarise(words = 
+    str_extract_all(
+    str_replace_all(str_to_lower(language_text), '[.,]', ""), '"(.*?)"'
+  )) %>%
+  unnest(cols = c(words))
