f0520c6c496ef2fe79692ce46568ac5e3e342e0b
Aman Bhargava <amanbhargava2001@gmail.com>
2021-11-14 12:03:05 +0530

    DD02

--- dd02_commonSenseMedia/common_sense_media_analysis.r
+++ dd02_commonSenseMedia/common_sense_media_analysis.r
@@ -5,19 +5,21 @@ library(patchwork) # Combining graphs
 library(ggtext)    # Using HTML in ggplot text
 library(gridExtra) # functions to work with pictures
 library(grid)
-library(corpus)
+library(Hmisc)
 library(extrafont)
 
 font_import("./resources/fonts/grand_royal/")
 loadfonts(device = "win", quiet = TRUE) 
 
-dd <- "dd02_common_sense_media"
+dd <- "dd02_commonSenseMedia"
 
 # Load data
 data <-
   read_csv(
     "https://raw.githubusercontent.com/thedivtagguy/movie-ratings/master/data/movie_data.csv"
   )
+genders <- read_csv(glue::glue("./{dd}/genre_gender_counts.csv")) %>% 
+  pivot_longer(cols = -c(X1), names_to = "genre", values_to = "count")
 
 data <- data %>%
   mutate(genres = strsplit(as.character(genres), ",")) %>%
@@ -39,7 +41,7 @@ mean <- data %>%
   labs(
     x = "Mean Difference Between Parent Recommended Age <br> and Kids Recommended Age",
     y = "Genres",
-    title = "Parents say <span style='font-size:30px;'>16</span>, kids say <span style='font-size:30px;'>12</span>",
+    title = "Parents say <span style='font-size:35px;'>16</span>, kids say <span style='font-size:35px;'>12</span>",
     subtitle = "Examining the mean difference in viewing ages <br> recommended by parents versus kids based on genre"
   ) +
   annotate(
@@ -83,12 +85,12 @@ mean <- data %>%
     plot.margin = unit(c(1, 2, 1, 2), "cm"),
     plot.title = element_markdown(
       margin = margin(t = 0.2, b = 0.2, unit = "cm"),
-      size = 26,
+      size = 15,
       family = "Grand Royal",
       face = "plain",
       colour = "#4C1E4F"
     ),
-    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), family="Raleway", face = "plain",colour = "#3F2F27"),
+    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), size = 13, lineheight = 1.3, family="Raleway", face = "plain",colour = "#3F2F27"),
     plot.background = element_rect(fill = "#FEE1C7", color = NA),
     axis.line = element_line(colour = "#3F2F27"),
     panel.grid = element_blank(),
@@ -102,7 +104,7 @@ mean <- data %>%
       face = "bold",
       colour = "#4C1E4F"
     ),
-    axis.text.y = element_markdown(colour = "#3F2F27"),
+    axis.text.y = element_markdown(colour = "#3F2F27", size = 12),
     axis.text.x = element_markdown(colour = "#3F2F27")
   )
 
@@ -138,7 +140,7 @@ language_plot <- language %>%
     colour = "#3F2F27"
     )+
   labs(title = "How bad are words?",
-       subtitle = "CSM rates movies on the kind of language they contain. <br> What are the most common words in each ranking?",
+       subtitle = "CSM rates, from 0 to 5, on the kind of language they use <br> What are the most common words in each ranking?",
        x = "Language rating",
        y = "Number of times mentioned")+
   theme_minimal()+
@@ -146,12 +148,12 @@ language_plot <- language %>%
     plot.margin = unit(c(1, 2, 1, 2), "cm"),
     plot.title = element_markdown(
       margin = margin(t = 0.2, b = 0.2, unit = "cm"),
-      size = 26,
+      size = 15,
       family = "Grand Royal",
       face = "plain",
       colour = "#4C1E4F"
     ),
-    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), lineheight = 1.2, family="Raleway", face = "plain",colour = "#3F2F27"),
+    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), size = 13, lineheight = 1.3, family="Raleway", face = "plain",colour = "#3F2F27"),
     plot.background = element_rect(fill = "#FEE1C7", color = NA),
     axis.line = element_line(colour = "#3F2F27"),
     panel.grid = element_blank(),
@@ -169,17 +171,74 @@ language_plot <- language %>%
     axis.text.x = element_markdown(colour = "#3F2F27", size = 15, family = "Raleway", face ="bold", margin = margin(t = 10))
   )
 
-mean + language_plot
 
-### Ratings by genre
-data %>% 
-  group_by(genres) %>% 
-  pivot_longer(c(diversity_rating, language_rating, positive_rating, role_models_rating, violence_rating, sex_rating, language_rating, consumerism_rating, drugs_rating), values_to = "rating", names_to = "category") %>% 
-  select(genres, category, rating) %>% 
-  ggplot(aes(x = genres, y = rating, fill = category)) + 
-  geom_bar(position = "dodge", stat= "identity")
+### Words in consumerism_text
+gender <- genders %>%
+  filter(count != 0) %>% 
+  mutate(X1 = capitalize(X1)) %>% 
+  mutate(X1 = factor(X1, levels = c("Male", "Female"))) %>% 
+  ggplot(aes(x = reorder(genre, -count), y = count, fill = X1)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  geom_text(aes(label = count), vjust = -0.5, colour = "#3F2F27", size = 3, position = position_dodge(.9)) +
+  scale_y_continuous(limits = c(0,160), expand = expansion(mult = c(0.002, .1))) +
+  scale_fill_manual(values = c("#E63946", "#457B9D")) +
+  theme_minimal() +
+  labs(
+    x = "Genre",
+    y = "Count",
+    fill = "",
+    title = "Who are the role models?",
+    subtitle = "CSM also reviews movies on whether or not they have characters who can be positive role models. <br> How does the gender of these role models vary with genre?"
+  ) +
+  theme(
+    plot.title = element_markdown(
+      margin = margin(t = 0.2, b = 0.2, unit = "cm"),
+      size = 15,
+      family = "Grand Royal",
+      face = "plain",
+      colour = "#4C1E4F",
+      hjust = 0.5
+    ),
+    plot.subtitle = element_markdown(margin = margin(b = 1, unit = "cm"), size = 13, hjust = 0.5, lineheight = 1.3, family="Raleway", face = "plain",colour = "#3F2F27"),
+    plot.background = element_rect(fill = "#FEE1C7", color = NA),
+    axis.line = element_line(colour = "#3F2F27"),
+    panel.grid = element_blank(),
+    axis.title.x = element_markdown(
+      face = "bold",
+      colour = "#4C1E4F"
+    ),
+    axis.title.y = element_markdown(
+      face = "bold",
+      colour = "#4C1E4F"
+    ),
+    legend.position = "bottom",
+    plot.caption = element_markdown(colour = "#3F2F27", size = 12),
+    axis.text.y = element_markdown(colour = "#3F2F27", size = 15),
+    axis.text.x = element_markdown(colour = "#3F2F27", angle = 60, vjust = 0.9, hjust = 0.95, size = 12)
+  )
 
+combined <- (mean + language_plot) / gender
+
+combined + plot_annotation(
+  title = 'Common Sense Media',
+  subtitle = 'Common Sense Media (CSM) uses various scales and categories to review and rate movies, <br> such as based on the kind of positive messages, role models, use of language, amount of violence and so on. <br> We can use this information to visualize things such as...',
+  caption = 'Source: Common Sense Media, 2021 <br> <b>Visualization: @thedivtagguy</b>'
+) & 
+  theme(
+    plot.title = element_markdown(
+      margin = margin(t = 0.2, b = 0.2, unit = "cm"),
+      size = 32,
+      family = "Grand Royal",
+      face = "plain",
+      colour = "#4C1E4F",
+      hjust = 0.5
+    ),
+    plot.margin = unit(c(1,1,1,1), "cm"),
+    plot.caption = element_markdown(size = 13, lineheight = 1.5),
+    plot.subtitle = element_markdown(margin = margin(t = 0.5, b=0.8, unit = "cm"), size = 20, hjust = 0.5, lineheight = 1.3, family="Raleway", face = "plain",colour = "#3F2F27"),
+    plot.background = element_rect(fill = "#FEE1C7", color = NA),
+  )
 # Save
 plot_name <- "common_sense"
-ggsave(glue::glue("./{plot_name}.png"), width = 40, height = 20, units = "cm")
+ggsave(glue::glue("./{dd}/{plot_name}.png"), width = 48, height = 56, units = "cm")
 
---
+++ dd02_commonSenseMedia/spacy_analysis.py
@@ -0,0 +1,61 @@
+import csv
+import spacy
+import pandas as pd
+
+# import gender guesser
+import gender_guesser.detector as gender
+
+d = gender.Detector()
+d.get_gender("Bob")
+# Read in the csv from a url and store it in a dataframe
+df = pd.read_csv('https://raw.githubusercontent.com/thedivtagguy/movie-ratings/master/data/movie_data.csv')
+
+
+# Explode df on genres
+df['genres'] = df['genres'].str.split(',')
+out = df.explode('genres')
+
+# Group by genres and select only the role_models_text column
+df_grouped = out.groupby('genres')['role_models_text'].apply(list).reset_index()
+
+nlp = spacy.load('en_core_web_sm')
+
+genre_entities = {}
+
+# For each genre, create a list of named entities
+for i in range(len(df_grouped)):
+    # For each genre, create a list of named entities
+    # Add genre to the dictionary
+    try:
+        genre_entities[df_grouped.iloc[i]['genres']] = []
+        for j in range(len(df_grouped.iloc[i]['role_models_text'])):
+            doc = nlp(df_grouped.iloc[i]['role_models_text'][j])
+            for ent in doc.ents:
+                if ent.label_ == 'PERSON':
+                    genre_entities[df_grouped.iloc[i]['genres']].append(ent.text)
+    except:
+        pass
+
+def gender_id(text):
+    return d.get_gender(text)
+
+# Iterate through genres in genre_entities and add count
+genre_counts = {}
+try:
+    for key, value in genre_entities.items():
+        genre_counts[key] = {}
+        male = 0
+        female = 0
+        for i in value:
+            if gender_id(i) == 'male':
+                male += 1
+            if gender_id(i) == 'female':
+                female += 1
+        genre_counts[key] = {'male': male, 'female': female}
+except:
+    pass            
+
+# Save dictionary to df
+gender_counts = pd.DataFrame(genre_counts)
+
+gender_counts.to_csv("genre_gender_counts.csv")
