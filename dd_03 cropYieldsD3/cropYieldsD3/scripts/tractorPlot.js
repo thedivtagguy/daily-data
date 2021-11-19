04df392b502cca0c2738e81572d2f7fbfb4e33c4
Aman Bhargava <amanbhargava2001@gmail.com>
2021-11-20 01:20:48 +0530

    Adding titles dd03

--- dd_03 cropYieldsD3/cropYieldsD3/scripts/tractorPlot.js
+++ dd_03 cropYieldsD3/cropYieldsD3/scripts/tractorPlot.js
@@ -1,16 +1,25 @@
 import * as d3 from "d3";
+import legendColor from '../../cropYieldsD3/node_modules/d3-svg-legend'
+
 const margin = {
   top: 20,
-  right: 20,
+  right: 50,
   bottom: 30,
-  left: 150,
+  left: 160,
 };
 const width = 960 - margin.left - margin.right;
 const height = 2500 - margin.top - margin.bottom;
 
+let info = d3
+    .select("#chartInfo")
+    .append("div")
+    .attr("class", "info");
+
 let chart = d3
   .select("#chart2")
   .append("div")
+  // Set id to chartArea
+  .attr("id", "chartArea")
   .classed("chart", true)
   .append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -27,19 +36,82 @@ let axis = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + ", 0)");
 
+let legend = d3
+    .select("#chart2")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", 40)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + ", 0)");
+
 // Load the data
 d3.csv("data/land_use.csv").then(function (data) {
-  // console.log(data);
+
+  // Read in data and various groups
   const years = Array.from(new Set(data.map((d) => d.year)));
   const countries = Array.from(new Set(data.map((d) => d.entity)));
   countries.reverse();
   const x = d3.scaleBand().range([0, width]).domain(years).padding(0.01);
 
-  const y = d3
-    .scaleBand()
-    .range([height, 0])
-    .domain(countries)
-    .padding(0.01);
+  // Create a tooltip
+  let tooltip = d3
+    .select("#chart2")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("color", "black")
+    .attr("class", "text-left")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px");
+
+ // Functions for tooltips
+  const mouseover = function (event, d) {
+    tooltip.style("opacity", 1);
+    d3.select(this)
+      .style("stroke", "black")
+      .style("stroke-width", "0.3px")
+      .style("opacity", 1);
+  };
+  const mousemove = function (event, d) {
+    let html =
+      `<div class='row'>
+    
+    <div class='col-md-12'>
+        <b>Country: </b>` +
+      d.entity +
+      `
+   </div>
+    <div class='col-md-12'>
+        <b>Year: </b>` +
+      d.year +
+      `
+    </div>
+    <div class='col-md-12'>
+        <b>Change in land use since previous year: </b>` +
+      d.cumChange +
+      `
+    </div>`;
+    tooltip
+      .html(html)
+      // Position the tooltip next to the cursor
+      .style("left", event.x / 2 + "px")
+      .style("top", (event.y > 300 ? event.y : event.y - 200) + "px")
+      .style("position", "absolute");
+  };
+
+
+  const mouseleave = function (event, d) {
+    tooltip.style("opacity", 0);
+    d3.select(this).style("stroke", "none").style("opacity", 0.8);
+  };
+
+
+  // Y scale
+  const y = d3.scaleBand().range([height, 0]).domain(countries).padding(0.01);
+
 
   // Only 10 years
   axis
@@ -63,6 +135,15 @@ d3.csv("data/land_use.csv").then(function (data) {
     .domain([0, d3.max(data, (d) => d.change)])
     .interpolator(d3.interpolateInferno);
 
+ // Add the chart title
+    info
+        .append("text")
+        .attr("x", (width / 2))
+        .attr("y", 0 - (margin.top / 2))
+        .attr("text-anchor", "middle")
+        .attr("class", "mb-5 text-2xl font-medium text-gray-600 capitalize")
+        .text("Change in land use since previous year");
+
   // add the squares
   chart
     .selectAll()
@@ -84,5 +165,13 @@ d3.csv("data/land_use.csv").then(function (data) {
     })
     .style("stroke-width", 4)
     .style("stroke", "none")
-    .style("opacity", 0.8);
+    .style("opacity", 0.8)
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
+
+
+
+
+    
 });
